2cb13a0d888794082fa58fc559728adaa0e9d035
Rafał Żelazko <rzelazko@mbp-ilikeiron.local>
2020-03-28 10:15:53 +0100

    Set valid primary color

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light mb-3">
     <div class="container px-sm-3">
-        <a class="nabar-brand bg-success text-white text-center p-2 d-inline-block my-2"
+        <a class="nabar-brand bg-primary text-white text-center p-2 d-inline-block my-2"
             href="{{ home_url('/') }}"><span class="lead">Polska Parafia</span><br /><small>Nicea Cannes Monaco</small></a>
         <button class="navbar-toggler" data-toggle="collapse" data-target="#navbar-primary-navigation">
             <span class="navbar-toggler-icon"></span>
