b55ef789e9d20b29dd8c97b05e50cfdca1fa9b6b
Rafał Żelazko <rzelazko@mbp-ilikeiron.local>
2020-03-29 11:44:11 +0200

    Add background to sidebar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,15 +2,16 @@
 <html {!! get_language_attributes() !!}>
   @include('partials.head')
   <body @php body_class() @endphp>
-    @php do_action('get_header') @endphp
-    @include('partials.header')
     <div class="wrap container" role="document">
-      <div class="content row">
+      @php do_action('get_header') @endphp
+      @include('partials.header')
+
+      <div class="content row ">
         <main class="main col-12 col-md">
           @yield('content')
         </main>
         @if (App\display_sidebar())
-          <aside class="sidebar col-12 col-md-4">
+          <aside class="sidebar col-12 col-md-4 bg-light">
             @include('partials.sidebar')
           </aside>
         @endif
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,78 +1,74 @@
-<div class="container">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light mb-3 justify-content-between">
-        <a class="nabar-brand bg-primary text-white text-center p-2 p-lg-4 d-inline-block my-2" href="{{ home_url('/') }}">
-            <img src="@asset('images/nicea-parafia-logo.png')" width="39" height="52" class="d-none d-lg-inline-block align-middle" alt="" />
-            <span class="d-inline-block align-middle">
-                <span class="lead d-block">Polska Parafia</span>
-                <small class="d-block">Nicea Cannes Monaco</small>
-            </span>
-        </a>
-        <button class="navbar-toggler" data-toggle="collapse" data-target="#navbar-primary-navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        @if (has_nav_menu('primary_navigation'))
-            {!! wp_nav_menu([
-                'theme_location' => 'primary_navigation',
-                'depth' => 2,
-                'container' => 'div',
-                'container_class' => 'collapse navbar-collapse',
-                'container_id' => 'navbar-primary-navigation',
-                'menu_class' => 'nav navbar-nav ml-auto',
-                'fallback_cb' => 'WP_Bootstrap_Navwalker::fallback',
-                'walker' => new WP_Bootstrap_Navwalker()
-            ]) !!}
-        @endif
-    </nav>
-</div>
+<nav class="navbar navbar-expand-lg navbar-light bg-light py-2 mb-3 justify-content-between">
+    <a class="nabar-brand bg-primary text-white text-center p-2 p-lg-4 d-inline-block my-2" href="{{ home_url('/') }}">
+        <img src="@asset('images/nicea-parafia-logo.png')" width="39" height="52" class="d-none d-lg-inline-block align-middle" alt="" />
+        <span class="d-inline-block align-middle">
+            <span class="lead d-block">Polska Parafia</span>
+            <small class="d-block">Nicea Cannes Monaco</small>
+        </span>
+    </a>
+    <button class="navbar-toggler" data-toggle="collapse" data-target="#navbar-primary-navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    @if (has_nav_menu('primary_navigation'))
+        {!! wp_nav_menu([
+            'theme_location' => 'primary_navigation',
+            'depth' => 2,
+            'container' => 'div',
+            'container_class' => 'collapse navbar-collapse',
+            'container_id' => 'navbar-primary-navigation',
+            'menu_class' => 'nav navbar-nav ml-auto',
+            'fallback_cb' => 'WP_Bootstrap_Navwalker::fallback',
+            'walker' => new WP_Bootstrap_Navwalker()
+        ]) !!}
+    @endif
+</nav>
 
 @if (is_home())
-<header class="container">
-    <div id="carouselNiceaParafia" class="carousel slide mb-3" data-ride="carousel">
-        <ol class="carousel-indicators">
-            <li data-target="#carouselNiceaParafia" data-slide-to="0" class="active"></li>
-            <li data-target="#carouselNiceaParafia" data-slide-to="1"></li>
-            <li data-target="#carouselNiceaParafia" data-slide-to="2"></li>
-        </ol>
-        <div class="carousel-inner">
-            <div class="carousel-item active carousel-image-1">
-                <div class="np-carousel-caption d-none d-md-flex row justify-content-end align-items-end">
-                    <div class="col-md-6 col-lg-5 np-carousel-caption-box py-3">
-                        <h5>Chemin de l'Energie Alpy Nadmorskie</h5>
-                        <p>
-                            A potem Bóg rzekł «Niech zbiorą się wody spod nieba w jedno miejsce i niech się ukaże
-                            powierzchnia sucha!»
-                        </p>
-                        <p class="small text-right">
-                            Rdz 1
-                        </p>
-                    </div>
-                </div>
-            </div>
-            <div class="carousel-item carousel-image-2">
-                <div class="np-carousel-caption d-none d-md-flex row justify-content-end align-items-end">
-                    <div class="col-md-6 col-lg-4 np-carousel-caption-box py-3">
-                        <h5>Lorem, ipsum.</h5>
-                        <p>Lorem ipsum dolor sit amet.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="carousel-item carousel-image-3">
-                <div class="np-carousel-caption d-none d-md-flex row justify-content-end align-items-end">
-                    <div class="col-md-6 col-lg-4 np-carousel-caption-box py-3">
-                        <h5>Lorem, ipsum.</h5>
-                        <p>Lorem ipsum dolor sit amet.</p>
-                    </div>
+<header id="carouselNiceaParafia" class="carousel slide mb-3" data-ride="carousel">
+    <ol class="carousel-indicators">
+        <li data-target="#carouselNiceaParafia" data-slide-to="0" class="active"></li>
+        <li data-target="#carouselNiceaParafia" data-slide-to="1"></li>
+        <li data-target="#carouselNiceaParafia" data-slide-to="2"></li>
+    </ol>
+    <div class="carousel-inner">
+        <div class="carousel-item active carousel-image-1">
+            <div class="np-carousel-caption d-none d-md-flex row justify-content-end align-items-end">
+                <div class="col-md-6 col-lg-5 np-carousel-caption-box py-3">
+                    <h5>Chemin de l'Energie Alpy Nadmorskie</h5>
+                    <p>
+                        A potem Bóg rzekł «Niech zbiorą się wody spod nieba w jedno miejsce i niech się ukaże
+                        powierzchnia sucha!»
+                    </p>
+                    <p class="small text-right">
+                        Rdz 1
+                    </p>
                 </div>
             </div>
         </div>
-        <a class="carousel-control-prev" href="#carouselNiceaParafia" role="button" data-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="sr-only">Poprzedni</span>
-        </a>
-        <a class="carousel-control-next" href="#carouselNiceaParafia" role="button" data-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="sr-only">Następny</span>
-        </a>
+        <div class="carousel-item carousel-image-2">
+            <div class="np-carousel-caption d-none d-md-flex row justify-content-end align-items-end">
+                <div class="col-md-6 col-lg-4 np-carousel-caption-box py-3">
+                    <h5>Lorem, ipsum.</h5>
+                    <p>Lorem ipsum dolor sit amet.</p>
+                </div>
+            </div>
+        </div>
+        <div class="carousel-item carousel-image-3">
+            <div class="np-carousel-caption d-none d-md-flex row justify-content-end align-items-end">
+                <div class="col-md-6 col-lg-4 np-carousel-caption-box py-3">
+                    <h5>Lorem, ipsum.</h5>
+                    <p>Lorem ipsum dolor sit amet.</p>
+                </div>
+            </div>
+        </div>
     </div>
+    <a class="carousel-control-prev" href="#carouselNiceaParafia" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Poprzedni</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselNiceaParafia" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Następny</span>
+    </a>
 </header>
 @endif
