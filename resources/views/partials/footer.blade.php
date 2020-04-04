dc99816279a8b2084e2a180eb09168c1fd0a526a
Rafał Żelazko <rzelazko@mbp-ilikeiron.local>
2020-04-04 19:34:43 +0200

    Basic footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,5 +1,40 @@
-<footer class="content-info">
+<footer class="content-info bg-light py-4 mt-4">
   <div class="container">
-    @php dynamic_sidebar('sidebar-footer') @endphp
+    <div class="row">
+      @php dynamic_sidebar('sidebar-footer') @endphp
+    </div>
+  </div>
+</footer>
+<footer class="bg-dark">
+  <div class="container px-md-0">
+    <nav class="navbar navbar-expand-lg navbar-dark px-0 py-2 mb-3 justify-content-between">
+      <a class="nabar-brand bg-primary text-white text-center p-2 d-inline-flex my-2 small" href="{{ home_url('/') }}">
+        <img src="@asset('images/nicea-parafia-logo.png')" width="21" height="28"
+          class="d-none d-lg-inline-block align-middle" alt="" />
+        <span class="d-inline-block align-middle">
+          <span class="d-block">Polska Parafia</span>
+          <small class="d-block">Nicea Cannes Monaco</small>
+        </span>
+      </a>
+      <button class="navbar-toggler" data-toggle="collapse" data-target="#navbar-primary-navigation">
+          <span class="navbar-toggler-icon"></span>
+      </button>
+      @if (has_nav_menu('primary_navigation'))
+          {!! wp_nav_menu([
+              'theme_location' => 'primary_navigation',
+              'depth' => 1,
+              'container' => 'div',
+              'container_class' => 'collapse navbar-collapse',
+              'container_id' => 'navbar-primary-navigation',
+              'menu_class' => 'nav navbar-nav ml-auto',
+              'fallback_cb' => 'WP_Bootstrap_Navwalker::fallback',
+              'walker' => new WP_Bootstrap_Navwalker()
+          ]) !!}
+      @endif
+      <a class="d-none d-lg-inline-flex ml-4" href="http://www.webperfekt.pl/">
+        <img src="@asset('images/webperfekt-logo.png')" height="24"
+          class="d-none d-lg-inline-block align-middle" alt="WebPerfekt.pl" />
+      </a>
+    </nav>
   </div>
 </footer>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,27 +1,29 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light py-2 mb-3 justify-content-between">
-    <a class="nabar-brand bg-primary text-white text-center p-2 p-lg-4 d-inline-block my-2" href="{{ home_url('/') }}">
-        <img src="@asset('images/nicea-parafia-logo.png')" width="39" height="52" class="d-none d-lg-inline-block align-middle" alt="" />
-        <span class="d-inline-block align-middle">
-            <span class="lead d-block">Polska Parafia</span>
-            <small class="d-block">Nicea Cannes Monaco</small>
-        </span>
-    </a>
-    <button class="navbar-toggler" data-toggle="collapse" data-target="#navbar-primary-navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    @if (has_nav_menu('primary_navigation'))
-        {!! wp_nav_menu([
-            'theme_location' => 'primary_navigation',
-            'depth' => 2,
-            'container' => 'div',
-            'container_class' => 'collapse navbar-collapse',
-            'container_id' => 'navbar-primary-navigation',
-            'menu_class' => 'nav navbar-nav ml-auto',
-            'fallback_cb' => 'WP_Bootstrap_Navwalker::fallback',
-            'walker' => new WP_Bootstrap_Navwalker()
-        ]) !!}
-    @endif
-</nav>
+<header>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light py-2 mb-3 justify-content-between">
+        <a class="nabar-brand bg-primary text-white text-center p-2 p-lg-4 d-inline-flex my-2" href="{{ home_url('/') }}">
+            <img src="@asset('images/nicea-parafia-logo.png')" width="39" height="52" class="d-none d-lg-inline-block align-middle" alt="" />
+            <span class="d-inline-block align-middle">
+                <span class="lead d-block">Polska Parafia</span>
+                <small class="d-block">Nicea Cannes Monaco</small>
+            </span>
+        </a>
+        <button class="navbar-toggler" data-toggle="collapse" data-target="#navbar-primary-navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        @if (has_nav_menu('primary_navigation'))
+            {!! wp_nav_menu([
+                'theme_location' => 'primary_navigation',
+                'depth' => 2,
+                'container' => 'div',
+                'container_class' => 'collapse navbar-collapse',
+                'container_id' => 'navbar-primary-navigation',
+                'menu_class' => 'nav navbar-nav ml-auto',
+                'fallback_cb' => 'WP_Bootstrap_Navwalker::fallback',
+                'walker' => new WP_Bootstrap_Navwalker()
+            ]) !!}
+        @endif
+    </nav>
+</header>
 
 @if (is_home())
 <header id="carouselNiceaParafia" class="carousel slide mb-3" data-ride="carousel">
