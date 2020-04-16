1ce8601961924f990bf828f0ca530e3ed5a24188
Rafał Żelazko <rzelazko@gmail.com>
2020-04-16 20:40:39 +0200

    Support for excerpts

--- resources/views/partials/content.blade.php
+++ resources/views/partials/content.blade.php
@@ -1,3 +1,16 @@
+@if (has_excerpt())
+<article @php post_class('post-excerpt card bg-primary text-white') @endphp>
+  <div class="card-body">
+    <h2 class="card-title"><a href="{{ get_permalink() }}">{!! get_the_title() !!}</a></h2>
+
+    @include('partials/entry-meta')
+    {!! the_excerpt() !!}
+
+    <a href="{{ get_permalink() }}" class="btn btn-light">{{__('Read more', 'sage')}}
+      <i class="fas fa-angle-double-right"></i></a>
+  </div>
+</article>
+@else
 <article @php post_class() @endphp>
   <header>
     <h2 class="entry-title"><a href="{{ get_permalink() }}">{!! get_the_title() !!}</a></h2>
@@ -5,6 +18,7 @@
   </header>
   @include('partials/post-thumbnail')
   <div class="entry-summary">
-    @php the_excerpt() @endphp
+    {!! the_excerpt() !!}
   </div>
 </article>
+@endif
