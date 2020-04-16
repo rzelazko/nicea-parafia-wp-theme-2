36b79b6476e0cf2ef0be037f425f0c3662a99a8f
Rafał Żelazko <rzelazko@gmail.com>
2020-04-16 22:03:11 +0200

    Add gallery images support

--- resources/views/partials/post-thumbnail.blade.php
+++ resources/views/partials/post-thumbnail.blade.php
@@ -2,4 +2,17 @@
     <a href="@php the_permalink() @endphp" alt="@php the_title_attribute() @endphp" class="entry-thumbnail">
         @php the_post_thumbnail('large', array('class' => 'img-fluid')) @endphp
     </a>
+@elseif (has_post_format('gallery'))
+    @php
+        $gallery_images = get_post_gallery_images();
+        $gallery_size = count($gallery_images);
+        $gallery_image = $gallery_size > 0 ? $gallery_images[0] : null;
+    @endphp
+
+    @isset($gallery_image)
+        <a href="@php the_permalink() @endphp" alt="@php the_title_attribute() @endphp" class="entry-thumbnail">
+            <img src="{{ $gallery_image }}" class="img-fluid" alt="" />
+        </a>
+        <p>{{ sprintf(__('Gallery contains %s images', 'sage'), $gallery_size) }}</p>
+    @endisset
 @endif
