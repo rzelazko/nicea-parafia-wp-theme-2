5785813d6f7214bc03404cfb4e3fc86c7c86fc89
Rafał Żelazko <rzelazko@gmail.com>
2020-04-16 17:56:30 +0200

    Init subpage header carousel

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -37,7 +37,7 @@
     <div class="carousel-inner">
         <div class="carousel-item active carousel-image-1">
             <div class="np-carousel-caption d-none d-md-flex row justify-content-end align-items-end">
-                <div class="col-md-6 col-lg-5 np-carousel-caption-box py-3">
+                <div class="col-md-6 col-lg-5 np-carousel-caption-box np-carousel-caption-bg-dark py-3">
                     <h5>Chemin de l&apos;Energie Alpy Nadmorskie</h5>
                     <p>
                         A potem Bóg rzekł «Niech zbiorą się wody spod nieba w jedno miejsce i niech się ukaże
@@ -51,7 +51,7 @@
         </div>
         <div class="carousel-item carousel-image-2">
             <div class="np-carousel-caption d-none d-md-flex row justify-content-end align-items-end">
-                <div class="col-md-6 col-lg-4 np-carousel-caption-box py-3">
+                <div class="col-md-6 col-lg-4 np-carousel-caption-box np-carousel-caption-bg-dark py-3">
                     <h5>Col de Villefranche, Nicea</h5>
                     <p>I mówili nawzajem do siebie: «Czy serce nie pałało w nas, kiedy rozmawiał z nami w drodze i Pisma nam wyjaśniał?»</p>
                     <p class="small text-right">Łk 24,32</p>
@@ -60,7 +60,7 @@
         </div>
         <div class="carousel-item carousel-image-3">
             <div class="np-carousel-caption d-none d-md-flex row justify-content-end align-items-end">
-                <div class="col-md-6 col-lg-4 np-carousel-caption-box py-3">
+                <div class="col-md-6 col-lg-4 np-carousel-caption-box np-carousel-caption-bg-dark py-3">
                     <h5>Kaplica, Plac Garibaldi, Nicea</h5>
                     <p>O, gwiazdo najświętsza wiedź nas w niebios kraj. I wieczną szczęśliwość, o Boże, nam daj.</p>
                     <p class="small text-right">Po górach dolinach</p>
@@ -69,7 +69,7 @@
         </div>
         <div class="carousel-item carousel-image-4">
             <div class="np-carousel-caption d-none d-md-flex row justify-content-end align-items-end">
-                <div class="col-md-6 col-lg-4 np-carousel-caption-box py-3">
+                <div class="col-md-6 col-lg-4 np-carousel-caption-box np-carousel-caption-bg-dark py-3">
                     <h5>Promenade des Anglais, Nicea</h5>
                     <p>Pan kiedyś stanął nad brzegiem; Szukał ludzi gotowych pójść za Nim; By łowić serca; Słów Bożych prawdą</p>
                     <p class="small text-right">Barka</p>
@@ -78,7 +78,7 @@
         </div>
         <div class="carousel-item carousel-image-5">
             <div class="np-carousel-caption d-none d-md-flex row justify-content-end align-items-end">
-                <div class="col-md-6 col-lg-4 np-carousel-caption-box py-3">
+                <div class="col-md-6 col-lg-4 np-carousel-caption-box np-carousel-caption-bg-dark py-3">
                     <h5>Val d'Allos, Alpy Prowansalskie</h5>
                     <p>Błogosław, duszo moja, Pana! O Boże mój, Panie, jesteś bardzo wielki! Odziany we wspaniałość i majestat, światłem okryty jak płaszczem. Rozpostarłeś niebo jak namiot, wzniosłeś swe komnaty nad wodami.</p>
                     <p class="small text-right">Ps 104</p>
@@ -94,5 +94,29 @@
         <span class="carousel-control-next-icon" aria-hidden="true"></span>
         <span class="sr-only">Następny</span>
     </a>
+</header>
+@else
+<header id="carouselNiceaParafia" class="carousel slide mb-3" data-ride="carousel">
+    <ol class="carousel-indicators">
+        <li data-target="#carouselNiceaParafia" data-slide-to="0" class="active"></li>
+        <li data-target="#carouselNiceaParafia" data-slide-to="1"></li>
+    </ol>
+    <div class="carousel-inner">
+        <div class="carousel-item carousel-item-subpage active carousel-image-sub-1">
+            <div class="np-carousel-caption d-none d-md-flex row justify-content-end align-items-end">
+                <div class="np-carousel-caption-bg-dark p-1 m-2">
+                    <div>Grand Braus - Alpy Nadmorskie</div>
+                </div>
+            </div>
+        </div>
+
+        <div class="carousel-item carousel-item-subpage carousel-image-sub-2">
+            <div class="np-carousel-caption d-none d-md-flex row justify-content-end align-items-end">
+                <div class="np-carousel-caption-bg-dark p-1 m-2">
+                    <p>Île Saint-Honorat</p>
+                </div>
+            </div>
+        </div>
+    </div>
 </header>
 @endif
